e2974deb1e297dfee17766d11bddb014ab4a6f1f
gruvin <gruvin@2fb994ee-a9a9-1d01-5096-f0de19865d37>
2010-12-04 22:10:27 +0000

    Updated Copyright text to comply with recommendations by the Free Software Foundation. (Can't hurt.)

--- trunk/FrSky_TerminalAppDelegate.m
+++ trunk/FrSky_TerminalAppDelegate.m
@@ -3,7 +3,23 @@
 //  FrSky Terminal
 //
 //  Created by Bryan on 28/11/10.
-//  Copyright 2010 Bryan J. Rentoul. All rights reserved.
+//  Copyright 2010 Bryan J. Rentoul.
+//  
+//  This file is part of the computer application software named FrSky Terminal.
+//  
+//  FrSky Terminal is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  FrSky Terminal is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with FrSky Terminal in a file named COPYING.  
+//	If not, see <http://www.gnu.org/licenses/>.
 //
 
 #import "FrSky_TerminalAppDelegate.h"
