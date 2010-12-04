506ad9cf32f8039c130d2a965d77fe198f460fac
gruvin <gruvin@2fb994ee-a9a9-1d01-5096-f0de19865d37>
2010-12-04 07:15:50 +0000

    Initial FrSky Terminal import

---
+++ FrSky_TerminalAppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  FrSky_TerminalAppDelegate.m
+//  FrSky Terminal
+//
+//  Created by Bryan on 28/11/10.
+//  Copyright 2010 NZ Hosting Ltd. All rights reserved.
+//
+
+#import "FrSky_TerminalAppDelegate.h"
+
+@implementation FrSky_TerminalAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application 
+}
+
+@end
