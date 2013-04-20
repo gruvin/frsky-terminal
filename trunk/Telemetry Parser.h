586d0fa6b6cdd5fa62a18e6bd02bc24c4284b652
gruvin@gmail.com <gruvin@gmail.com@2fb994ee-a9a9-1d01-5096-f0de19865d37>
2013-04-20 11:31:34 +0000

    More minor changes -- including work in the previous commit to make private methods and properties actually private, by moving them from their .h file previous locations, to a separate @interfacce section atop each of the two .m files.

--- trunk/Telemetry Parser.h
+++ trunk/Telemetry Parser.h
@@ -14,37 +14,6 @@
 #include <libgen.h>     /* for basename() */
 #include <IOKit/serial/IOSerialKeys.h> /* For retrieving list of available serial ports */
 
-
-struct FrskyHubData {
-    int16_t  gpsAltitude_bp;   // before punct
-    int16_t  temperature1;     // -20 .. 250 deg. celcius
-    uint16_t rpm;              // 0..60,000 revs. per minute
-    uint16_t fuelLevel;        // 0, 25, 50, 75, 100 percent
-    int16_t  temperature2;     // -20 .. 250 deg. celcius
-    uint16_t volts;            // 1/500V increments (0..4.2V)
-    int16_t  gpsAltitude_ap;   // after punct
-    int16_t  baroAltitude;     // 0..9,999 meters
-    uint16_t gpsSpeed_bp;      // before punct
-    uint16_t gpsLongitude_bp;  // before punct
-    uint16_t gpsLatitude_bp;   // before punct
-    uint16_t gpsCourse_bp;     // before punct (0..359.99 deg. -- seemingly 2-decimal precision)
-    uint8_t  day;
-    uint8_t  month;
-    uint16_t year;
-    uint8_t  hour;
-    uint8_t  min;
-    uint16_t sec;
-    uint16_t gpsSpeed_ap;
-    uint16_t gpsLongitude_ap;
-    uint16_t gpsLatitude_ap;
-    uint16_t gpsCourse_ap;
-    uint16_t gpsLongitudeEW;   // East/West
-    uint16_t gpsLatitudeNS;    // North/South
-    int16_t  accelX;           // 1/256th gram (-8g ~ +8g)
-    int16_t  accelY;           // 1/256th gram (-8g ~ +8g)
-    int16_t  accelZ;           // 1/256th gram (-8g ~ +8g)
-};
-
 #define FRSKY_TELEM_BUFFER_SIZE 1024
 #define FRSKY_USER_DATA_BUFFER_SIZE 255
 
@@ -97,6 +66,36 @@ struct FrskyLinkData {
     unsigned char frskyA2Value;                         // Holds most recently parsed Fr-Sky Receiver A2 analogue input port value
     unsigned char frskyRSSI1;
     unsigned char frskyRSSI2;
+};
+
+struct FrskyHubData {
+    int16_t  gpsAltitude_bp;   // before punct
+    int16_t  temperature1;     // -20 .. 250 deg. celcius
+    uint16_t rpm;              // 0..60,000 revs. per minute
+    uint16_t fuelLevel;        // 0, 25, 50, 75, 100 percent
+    int16_t  temperature2;     // -20 .. 250 deg. celcius
+    uint16_t volts;            // 1/500V increments (0..4.2V)
+    int16_t  gpsAltitude_ap;   // after punct
+    int16_t  baroAltitude;     // 0..9,999 meters
+    uint16_t gpsSpeed_bp;      // before punct
+    uint16_t gpsLongitude_bp;  // before punct
+    uint16_t gpsLatitude_bp;   // before punct
+    uint16_t gpsCourse_bp;     // before punct (0..359.99 deg. -- seemingly 2-decimal precision)
+    uint8_t  day;
+    uint8_t  month;
+    uint16_t year;
+    uint8_t  hour;
+    uint8_t  min;
+    uint16_t sec;
+    uint16_t gpsSpeed_ap;
+    uint16_t gpsLongitude_ap;
+    uint16_t gpsLatitude_ap;
+    uint16_t gpsCourse_ap;
+    uint16_t gpsLongitudeEW;   // East/West
+    uint16_t gpsLatitudeNS;    // North/South
+    int16_t  accelX;           // 1/256th gram (-8g ~ +8g)
+    int16_t  accelY;           // 1/256th gram (-8g ~ +8g)
+    int16_t  accelZ;           // 1/256th gram (-8g ~ +8g)
 };
 
 @protocol TelemtryParserDelegate <NSObject>
